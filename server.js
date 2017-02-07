bc19ed784bfc05ab27f36bd6c7d34a986ee0fe0c
Pyry Pajunpää <pymapa@utu.fi>
2017-02-07 12:09:16 +0200

    serving simple html-file

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 var express = require('express')
 var app = express()
 
+app.use(express.static('public'));
+
+
+app.set('port', process.env.PORT || 3000);
+
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+    res.send("Koodikerho lähettää tekstiä!");
+//   res.sendFile('views/index.html' , { root : __dirname});
 })
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+app.listen(app.get('port'), function () {
+  console.log('Node-express server running on port ' + app.get('port') + "!"); 
 })
