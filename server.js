44efa8cecbfa4daf411a4555c76600db7d4a6cbb
Pyry Pajunpää <pymapa@utu.fi>
2017-02-07 12:14:23 +0200

    added a cat2

--- server.js
+++ server.js
@@ -2,15 +2,17 @@ var express = require('express')
 var app = express()
 
 app.use(express.static('public'));
+app.use(express.static('views'));
 
 
 app.set('port', process.env.PORT || 3000);
 
 app.get('/', function (req, res) {
-    res.send("Koodikerho lähettää tekstiä!");
-//   res.sendFile('views/index.html' , { root : __dirname});
+    // res.send("Koodikerho lähettää tekstiä!");
+    // res.sendFile('views/index.html' , { root : __dirname});
+    // res.sendFile('index.html');
 })
 
 app.listen(app.get('port'), function () {
-  console.log('Node-express server running on port ' + app.get('port') + "!"); 
+    console.log('Node-express server running on port ' + app.get('port') + "!");
 })
