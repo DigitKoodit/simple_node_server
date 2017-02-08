b82ed5184fcaa008004220d93a966836e071f2b3
Pyry Pajunpää <pyry.pajunpaa@affecto.com>
2017-02-08 17:41:28 +0200

    toggle image working

--- public/js/script.js
+++ public/js/script.js
@@ -1,6 +1,11 @@
 showImage = function() {
     $("#funny_image").removeClass("hidden");
     $("#showImage_button").addClass("hidden");
-   /* $("#hideImage_button").addClass("hidden");
-    $("#funny_image").addClass("hidden");*/
+    $("#hideImage_button").removeClass("hidden");
+}
+
+hideImage = function() {
+    $("#funny_image").addClass("hidden");
+    $("#showImage_button").removeClass("hidden");
+    $("#hideImage_button").addClass("hidden");
 }
