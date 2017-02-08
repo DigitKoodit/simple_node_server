f96afa00161d8b4447b04270685eb391bcbcc51f
Pyry Pajunpää <pymapa@utu.fi>
2017-02-08 18:41:42 +0200

    colors

--- server.js
+++ server.js
@@ -7,8 +7,8 @@ app.use(express.static('views'));
 
 app.set('port', process.env.PORT || 3000);
 
-app.get('/', function (req, res) {
-    res.sendFile('index.html');
+app.get('/', function (request, response) {
+    response.sendFile('index.html');
 })
 
 app.listen(app.get('port'), function () {
