2543404c4ee1a10158b90d9be22f43c376a25219
Pyry Pajunpää <pymapa@utu.fi>
2017-02-08 18:18:16 +0200

    turhat kommentit poistettu

--- server.js
+++ server.js
@@ -8,11 +8,10 @@ app.use(express.static('views'));
 app.set('port', process.env.PORT || 3000);
 
 app.get('/', function (req, res) {
-    // res.send("Koodikerho lähettää tekstiä!");
-    // res.sendFile('views/index.html' , { root : __dirname});
     res.sendFile('index.html');
 })
 
 app.listen(app.get('port'), function () {
+    console.log("jeejee toimii");
     console.log('KoodiKerho server running on port ' + app.get('port') + "!");
 })
