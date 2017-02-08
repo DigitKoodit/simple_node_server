70610dae417342d39227779a3aa826f523496c7b
Pyry Pajunpää <pyry.pajunpaa@affecto.com>
2017-02-08 17:32:31 +0200

    showimage

---
+++ public/js/script.js
@@ -0,0 +1,6 @@
+showImage = function() {
+    $("#funny_image").removeClass("hidden");
+    $("#showImage_button").addClass("hidden");
+   /* $("#hideImage_button").addClass("hidden");
+    $("#funny_image").addClass("hidden");*/
+}
