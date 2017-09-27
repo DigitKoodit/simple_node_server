d14ed0f566d5bec3e47d9b4093b2d5b8cd4ffd00
Pyry Pajunpää <pymapa@utu.fi>
2017-09-27 18:39:15 +0300

    commented stuff

--- public/js/script.js
+++ public/js/script.js
@@ -7,15 +7,15 @@
 $(document).ready(function () {
     console.warn('document is ready.');
 
-    $.get('/employee/getEmployees', function (data, status) {
+    /* $.get('/employee/getEmployees', function (data, status) {
         // console.log(data);
         renderTable(data);
-    })
+    }) */
 
-    renderTable = function (data) {
+    /* renderTable = function (data) {
         for (var i = 0; i < data.length; i++) {
             $('#employee-table').find('tbody')
                 .append('<tr><td>' + data[i].first_name + '</td><td>' + data[i].last_name + '</td><td>' + data[i].email + '</td></tr>');
         }
-    }
+    } */
 })
