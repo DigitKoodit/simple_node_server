0ca62a97e3ae5024be8bd1fc70ddade80e5f216c
Pyry Pajunpää <pymapa@utu.fi>
2017-09-27 18:15:44 +0300

    script and css in use

--- public/js/script.js
+++ public/js/script.js
@@ -4,18 +4,18 @@
 
 
 
-$(document).ready(function() {
+$(document).ready(function () {
     console.warn('document is ready.');
 
-    $.get('/employee/getEmployees', function(data, status) {
+    $.get('/employee/getEmployees', function (data, status) {
         // console.log(data);
         renderTable(data);
     })
 
-    renderTable = function(data) {
-        for(var i=0; i<data.length; i++) {
+    renderTable = function (data) {
+        for (var i = 0; i < data.length; i++) {
             $('#employee-table').find('tbody')
-            .append('<tr><td>' + data[i].first_name + '</td><td>' + data[i].last_name + '</td><td>' + data[i].email + '</td></tr>');
+                .append('<tr><td>' + data[i].first_name + '</td><td>' + data[i].last_name + '</td><td>' + data[i].email + '</td></tr>');
         }
     }
 })
